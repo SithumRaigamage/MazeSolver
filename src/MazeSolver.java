bcb47c58ec797613c0f75959a380e1238e4564e8
SithumRaigamage <sraig2002@gmail.com>
2024-04-16 11:02:14 +0530

    added the method to find S and F of any pattern

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,32 +1,38 @@
 
-
+import java.util.*;
 public class MazeSolver {
 
-    
     // initialized variables
-    private char [][] maze;
-    private int StartRow;
-    private int StartCol;
-    private int endRow;
-    private int endCol;
-    private int numRows;
-    private int numCols;
-    private int[] [] directions ={{-1,0},{1,0},{0,-1},{0,1}};
-    private final Point[][] predecessor;
-    private final boolean[][] visited;
+    private char[][] maze;
+    private int startRow, startCol, endRow, endCol;
+    private int numRows, numCols;
+    private boolean[][] visited;
+    private int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private Point[][] predecessor;
 
     //constructor
-    public MazeSolver(char [][] maze){
-        this.maze=maze;
-        this.numRows=maze.length;
-        this.numCols=maze[0].length;
+    public MazeSolver(char[][] maze) {
+        this.maze = maze;
+        this.numRows = maze.length;
+        this.numCols = maze[0].length;
         this.visited = new boolean[numRows][numCols];
         this.predecessor = new Point[numRows][numCols];
         findStartAndEnd();
-
     }
 
     private void findStartAndEnd() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (maze[i][j] == 'S') {
+                    startRow = i;
+                    startCol = j;
+                }
+                if (maze[i][j] == 'F') {
+                    endRow = i;
+                    endCol = j;
+                }
+            }
+        }
     }
 
 
