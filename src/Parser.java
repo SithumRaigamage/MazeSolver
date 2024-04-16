d8328691ed2a0bbdbf88e6fe41e28337d75dce54
SithumRaigamage <sraig2002@gmail.com>
2024-04-16 18:49:23 +0530

    Parser Class created
    Parser class created to read the different patterns from the txt file

---
+++ src/Parser.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Parser {
+    public static char[][] parseFile(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        List<String> lines = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (!line.isEmpty()) {
+                // Replace '0' with 'O' for obstacles
+                line = line.replace('0', 'O');
+                lines.add(line);
+            }
+        }
+        scanner.close();
+
+        char[][] maze = new char[lines.size()][];
+        for (int i = 0; i < lines.size(); i++) {
+            maze[i] = lines.get(i).toCharArray();
+        }
+
+        return maze;
+    }
+}
