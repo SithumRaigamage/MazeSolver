67dcfff7f0e3f051f9cb3209979af4963337ee1e
SithumRaigamage <sraig2002@gmail.com>
2024-04-16 18:52:57 +0530

    added a maze2.txt
    sample tested by putting the original pattern by putting into a txt file

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
         try {
-            String filename = "filename.txt"; // Update with the correct path
+            String filename = "maze1.txt"; // Update with the correct path
             char[][] maze = Parser.parseFile(filename);
             MazeSolver solver = new MazeSolver(maze);
+            System.out.println(" ");
             System.out.println("Breadth-First Search:");
             if (!solver.solveBFS()) {
                 System.out.println("No solution could be found.");
