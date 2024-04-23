bf084f555ffe30cf884a7c9336d86c5dd6a20493
SithumRaigamage <sraig2002@gmail.com>
2024-04-23 13:40:15 +0530

    Coordinates Class implemented
    variables changed

--- MazeSolverA*/src/MazeSolver.java
+++ MazeSolverA*/src/MazeSolver.java
@@ -142,18 +142,5 @@ public class MazeSolver {
                 maze[row][col] != '0';
     }
 
-    private static class Coordinates {
-        int x, y;
-        String direction;
-        int g; // Cost to reach this node
-        int h; // Heuristic (estimated cost to the goal)
 
-        Coordinates(int x, int y, String direction, int g, int h) {
-            this.x = x;
-            this.y = y;
-            this.direction = direction;
-            this.g = g;
-            this.h = h;
-        }
-    }
 }
